7e588e5028fc640b14109cb75f0a0b843c72a9f6
jwb <butter_007@163.com>
2018-07-27 20:17:26 +0800

    存储添加页面的数据有暂时不能存储到数据库

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, session, redirect, url_for, current_app, request
 import json
-
-from app.main.form import BudgetForm
+from ..models import Teaminfo, Itemfee
+from .. import db
 from ..models import StandardFee
 from . import main
 
@@ -24,12 +24,58 @@ def add():
         feeList.append(item.extrafee)
         countryFeeDict[item.id] = feeList
     jsonstr = json.dumps(countryFeeDict)
-    return render_template('add01.html', jsonstr=jsonstr,data=countryFeeDict.items())
+    return render_template('add01.html', jsonstr=jsonstr, data=countryFeeDict.items())
 
 
-@main.route("/addexecute", methods=['GET','POST'])
+@main.route("/addexecute", methods=['GET', 'POST'])
 def addexecute():
-    print(request.form['peoplesname'])
-    print(request.form['peoples'])
-    print(request.form['days'])
+    teaminfo = Teaminfo(peoplesname=request.form.get('peoplesname', ''),
+                        peoples=request.form.get('peoples', 0),
+                        single=request.form.get('single', 0),
+                        days=request.form.get('days', 0),
+                        expectdate=request.form.get('expectdate', ''),
+                        area1=request.form.get('area1', ''),
+                        area1days=request.form.get('area1days', 0),
+                        cashtype1=request.form.get('cashtype1', ''),
+                        area2=request.form.get('area2', ''),
+                        area2days=request.form.get('area2days', 0),
+                        cashtype2=request.form.get('cashtype2', ''),
+                        area3=request.form.get('area3', ''),
+                        area3days=request.form.get('area3days', 0),
+                        cashtype3=request.form.get('cashtype3', ''))
+    print(request.form.get('area2days', 0))
+    print(1)
+    itemfee = Itemfee(fee12=float(request.form.get('fee12', 0)),
+                      fee13=float(request.form.get('fee13', 0)),
+                      fee14=request.form.get('fee14', ''),
+                      fee22=float(request.form.get('fee22', 0)),
+                      fee23=float(request.form.get('fee23', 0)),
+                      fee24=request.form.get('fee24', ''),
+                      fee32=float(request.form.get('fee32', 0)),
+                      fee33=float(request.form.get('fee33', 0)),
+                      fee34=request.form.get('fee34', ''),
+                      fee42=float(request.form.get('fee42', 0)),
+                      fee43=float(request.form.get('fee43', 0)),
+                      fee44=request.form.get('fee44', ''),
+                      fee52=float(request.form.get('fee52', 0)),
+                      fee53=float(request.form.get('fee53', 0)),
+                      fee54=request.form.get('fee54', ''),
+                      fee62=float(request.form.get('fee62', 0)),
+                      fee63=float(request.form.get('fee63', 0)),
+                      fee64=request.form.get('fee64', ''),
+                      fee72=float(request.form.get('fee72', 0)),
+                      fee73=float(request.form.get('fee73', 0)),
+                      fee74=request.form.get('fee74', ''),
+                      fee82=float(request.form.get('fee82', 0)),
+                      fee83=float(request.form.get('fee83', 0)),
+                      fee84=request.form.get('fee84', ''),
+                      fee92=float(request.form.get('fee92', 0)),
+                      fee93=float(request.form.get('fee93', 0)),
+                      fee94=request.form.get('fee94', ''),
+                      fee102=float(request.form.get('fee102', 0)),
+                      fee103=float(request.form.get('fee102', 0)),
+                      fee104=request.form.get('fee103', ''),
+                      teaminfo=teaminfo)
+    db.session.add(teaminfo)
+    db.session.add(itemfee)
     return redirect(url_for('main.index'))
